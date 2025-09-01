f707c62be06cc695453e4397621791f5903b2d3b
midhun <midhuntr7356@gmail.com>
2025-09-01 18:02:53 +0530

    feat: setup routes for login and registration pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
+import { Routes,Route } from 'react-router-dom'
+import Login from './Authentication/Login'
+import Registration from './Authentication/Registration'
 
 function App() {
   
 
   return (
     <>
-      
+      <Routes>
+        <Route path='/log' element={<Login/>}/>
+        <Route path='/reg' element={<Registration/>}/>
+      </Routes>
     </>
   )
 }
